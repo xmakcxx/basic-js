73d1823ac776b463bb242ef0ed1f2acf25b4d1c1
xmakcxx <xmakcxx@mail.ru>
2020-10-31 00:23:03 +0300

    fix new task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,19 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity != "string" || sampleActivity < 1 || sampleActivity > MODERN_ACTIVITY) {
+      return false;
+    }
+  
+    let a = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let age = a / k;
+  
+    if (isNaN(age) == true) {
+      return false;
+    }
+    else {
+      return Math.ceil(age);
+    }
+  };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(memb) {
+
+
+  let teamName = '';
+
+  if (!Array.isArray(memb)) {
+    return false;
+  }
+
+  for (let item of memb) {
+
+    if (typeof item != "string") {
+      continue;
+    }
+
+    item = item.trim();
+
+    teamName += item[0];
+  }
+
+  return teamName.toUpperCase().split('').sort().join('');
+}
